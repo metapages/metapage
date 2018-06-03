9e9160220a392ea61e1f7401fc824a73a25d4179
Dion Amago <dion@transition9.com>
2018-06-02 17:43:15 -0700

    minor

--- docs/_tools/metaframe-resize/index.js
+++ docs/_tools/metaframe-resize/index.js
@@ -3,12 +3,31 @@
 
 var metapage = new Metapage({debug:false});
 
-//Hard coding DCC for now
-var metaframe = metapage.createIFrame('http://0.0.0.0:4000/metapage/metaframes/passthrough/');
-var metaframeDiv = document.getElementById("iframe-wrapper");
-metaframeDiv.appendChild(metaframe.iframe);
 
-new ResizeObserver(function outputsize() {
-	document.getElementById('size-display').innerHTML = `Size: ${metaframe.iframe.offsetWidth}x${metaframe.iframe.offsetHeight}`;
-}).observe(metaframeDiv)
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+console.log(urlParams);
+
+if (urlParams && urlParams.url) {
+	//Hard coding DCC for now
+	var metaframe = metapage.createIFrame(urlParams.url);
+	var metaframeDiv = document.getElementById("iframe-wrapper");
+	metaframeDiv.appendChild(metaframe.iframe);
+
+	new ResizeObserver(function outputsize() {
+		document.getElementById('size-display').innerHTML = `Size: ${metaframe.iframe.offsetWidth}x${metaframe.iframe.offsetHeight}`;
+	}).observe(metaframeDiv);
+}
+
+
 
