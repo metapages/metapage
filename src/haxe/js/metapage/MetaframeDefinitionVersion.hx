73582c183154375f7989ba173999466622c96eda
Dion Amago <dion@transition9.com>
2018-08-16 11:35:40 -0700

    Minor versioning fixes/changes

--- src/haxe/js/metapage/MetaframeDefinitionVersion.hx
+++ src/haxe/js/metapage/MetaframeDefinitionVersion.hx
@@ -3,5 +3,7 @@ package js.metapage;
 @:enum
 abstract MetaframeDefinitionVersion(String) from String {
 	var Alpha = "alpha";
+	var Alpha2 = "0.1-alpha";
+	var Beta = "0.1-beta";
 	var V1 = "1";
 }
--- src/haxe/js/metapage/v1/MetaframeInstance.hx
+++ src/haxe/js/metapage/v1/MetaframeInstance.hx
@@ -3,9 +3,18 @@ package js.metapage.v1;
 //Just needed for the metaframe editor, it can be
 //editing metaframes that are not yet instances,
 //so don't have e.g. ids
+//Also is the primary document stored in the db
 typedef MetaframeInstanceAnonymous = {
 	var url :String;
 	var metaframe :MetaframeDefinition;
+	@:optional var screenshotUrl :String;
+	@:optional var updatedAt :Date;
+}
+
+typedef MetaframeInstance = { >MetaframeInstanceAnonymous,
+	//This id is only used when the metaframe is part of
+	//a metapage, i.e. it's an "instance" of the base metaframe
+	var id :MetaframeId;
 	//This is starting data, a way to configure and
 	//save metaframe instance state independent of pipes
 	//and not part of the actual static definition.
@@ -13,10 +22,4 @@ typedef MetaframeInstanceAnonymous = {
 	//metaframe:MetaframeDefinition
 	@:optional var inputs :Array<PipeUpdateBlob>;
 	@:optional var outputs :Array<PipeUpdateBlob>;
-}
-
-typedef MetaframeInstance = { >MetaframeInstanceAnonymous,
-	var id :MetaframeId;
-	@:optional var screenshotUrl :String;
-	@:optional var updatedAt :Date;
 }
