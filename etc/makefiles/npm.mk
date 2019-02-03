127860e04f594826526c46a0325c3f9af27426d3
Dion Amago <dion@transition9.com>
2019-02-02 21:40:24 -0800

    fix

--- etc/makefiles/npm.mk
+++ etc/makefiles/npm.mk
@@ -4,16 +4,16 @@ help-npm: help-impl-npm ## Print makefile update commands
 # This expects NPM_TOKEN in the form: //registry.npmjs.org/:_authToken=XXX
 # as this is what is directly dumped to the ~/.npmrc file (if it doesn't exist in that file)
 .PHONY: npm-publish
-npm-publish: guard-env-NPM_TOKEN ###npm NPM publish the package internally, for testing
-	@echo "PUBLISHING ${VERSION}"
+npm-publish: guard-env-NPM_TOKEN guard-env-NPM_VERSION ###npm NPM publish the package internally, for testing
+	@echo "PUBLISHING ${NPM_VERSION}"
 	@if ! grep --no-messages -q "${NPM_TOKEN}" "${HOME}/.npmrc" ; \
-		then echo "${NPM_TOKEN}" > ${HOME}/.npmrc ; \
+		then echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc ; \
 	fi
 	for name in "metaframe" "metapage" ; do \
 		rm -rf ${BASE_DIST}/$${name} ; \
 		mkdir -p ${BASE_DIST}/$${name} ; \
 		cp docs/js/$${name}.js ${BASE_DIST}/$${name}/index.js ; \
-		cat etc/npm/package.json | jq ". .version = \"${VERSION}\" | .name = \"$${name}\" | .main = \"$${name}.js\"" > ${BASE_DIST}/$${name}/package.json ; \
+		cat etc/npm/package.json | jq ". .version = \"${NPM_VERSION}\" | .name = \"$${name}\" | .main = \"$${name}.js\"" > ${BASE_DIST}/$${name}/package.json ; \
 		pushd ${BASE_DIST}/$${name} ; \
 		npm publish . || exit 1 ; \
 		popd ; \
