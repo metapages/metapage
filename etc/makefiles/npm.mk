574a77826b823748d0069c6125a7f43766d0c74d
Dion Amago <dion@transition9.com>
2019-02-03 17:23:17 -0800

    fixed

--- etc/makefiles/npm.mk
+++ etc/makefiles/npm.mk
@@ -7,16 +7,17 @@ help-npm: help-impl-npm ## Print makefile update commands
 		cat "${HELP_NPM}" ; \
 	fi
 
-NPM_VERSION ?= '0.0.16-${RANDOM}'
+NPM_VERSION ?= 0.0.16-${RANDOM}
 .PHONY: npm-publish
 npm-publish: guard-env-NPM_TOKEN guard-env-NPM_VERSION ###npm NPM publish the packages (metaframe+metapage)
 	@echo "PUBLISHING npm version ${NPM_VERSION}"
 	@rm -rf ${BASE_DIST}
 	@npx webpack --mode=production
 	@for name in "metaframe" "metapage" ; do \
-		cat etc/npm/package.json | jq ". .version = \"${NPM_VERSION}\" | .name = \"$${name}\" | .main = \"$${name}.js\"" > ${BASE_DIST}/$${name}/package.json ; \
+		cat package.json | jq ". .version = \"${NPM_VERSION}\" | .name = \"$${name}\"" > ${BASE_DIST}/$${name}/package.json ; \
 		pushd ${BASE_DIST}/$${name} ; \
 		echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc ; \
+		ln -s ${PWD}/node_modules . ; \
 		npm publish . || exit 1 ; \
 		popd ; \
 	done
