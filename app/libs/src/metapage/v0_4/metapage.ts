4d4753b4901d9f078e514ac475cd34f3f564f17e
Dion Amago <dion@transition9.com>
2024-02-18 14:31:14 -0800

    Add sha256 to metapage.meta

--- app/libs/src/metapage/v0_4/metapage.ts
+++ app/libs/src/metapage/v0_4/metapage.ts
@@ -1,34 +1,39 @@
-import { VersionsMetapage } from "./versions";
-import { MetapageId, Url } from "./core";
-import { MetaframeInstance } from "./metaframe";
+import {
+  MetapageId,
+  Url,
+} from './core';
+import { MetaframeInstance } from './metaframe';
+import { VersionsMetapage } from './versions';
 
 export interface MetapageOptions {
   id?: MetapageId;
   color?: string;
 }
 
-export type MetapageMetadata = {
-  name?: string;
-  description?: string;
-  author?: string;
-  image?: string;
-  keywords?: string[];
+export type MetapageMetadata = Partial<{
+  name: string;
+  description: string;
+  author: string;
+  image: string;
+  keywords: string[];
   // the idea is there *could* be different ways of displaying the metapage, so we store the preferred ways here
   // you cannot really bake in which one is the "default" since that is not under our control nor should we care
   // but we can have preferences
-  layouts?: {
+  layouts: {
     [key in string]: any;
   };
-  // layout?: MetapageMetadataLayout;
-};
+  // digest of MetapageDefinitionV3 EXCEPT the "meta" field
+  // because it's metadata and also contains this hash
+  sha256: string;
+}>;
 
 export interface MetapageDefinitionV3 {
   id?: MetapageId;
   // Best to require this even if annoying to users. It's like the docker-compose.yml version. Human velocity changes (slow but steady)
   version: VersionsMetapage;
+  // the core of this configuration: key to metaframes and their inputs
   metaframes: { [key: string]: MetaframeInstance };
-  // The plugin URLs point to the path containing a MetaframeInstance JSON
-  // It's an array because it needs to be sorted, but currently don't allow duplicate plugin URLs
+  // meta is optional metadata: name, layouts, tags, etc
   meta?: MetapageMetadata;
   // The plugin URLs point to the path containing a MetaframeInstance JSON
   // It's an array because it needs to be sorted, but currently don't allow duplicate plugin URLs
