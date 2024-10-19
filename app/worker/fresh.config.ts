dfd1b9cdf864de4280e2ae0f9511da2305035a8b
Dion Amago <dion@transition9.com>
2024-10-18 17:50:04 -0700

    checkpoint

--- app/worker/fresh.config.ts
+++ app/worker/fresh.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
+const certPath = ".certs/local-cert.pem";
+const keyPath = ".certs/local-key.pem";
+const cert = Deno.statSync(certPath).isFile  ? Deno.readTextFileSync(certPath) : undefined;
+const key = Deno.statSync(keyPath).isFile ? Deno.readTextFileSync(keyPath) : undefined;
+
 export default defineConfig({
   plugins: [tailwind()],
+  server: {
+    cert,
+    key,
+    port: parseInt(Deno.env.get("APP_PORT") || "8000"),
+    hostname: "0.0.0.0",
+  },
 });
--- app/worker/routes/_app.tsx
+++ app/worker/routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>metapage-lib-server</title>
+        <title>worker</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
