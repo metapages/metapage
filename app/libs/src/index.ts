253c1fce8628f39782954f570619ac67a56d1a3a
Dion Amago <dion@transition9.com>
2024-10-31 10:21:31 -0700

    Missing export

--- app/libs/src/index.ts
+++ app/libs/src/index.ts
@@ -3,7 +3,7 @@ export * from "./metapage/Metaframe";
 export * from "./metapage/MetapageTools";
 export * from "./metapage/Shared";
 export * from "./metapage/MetapageIFrameRpcClient";
-export * from "./metapage/v0_4";
+export * from "./metapage/v1";
 export * from "./metapage/data";
 export * from "./metapage/conversions";
 export * from "./metapage/versions";
