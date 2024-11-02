0d8834fc5db428bad4a411163d25547ec339a6e7
Dion Amago <dion@transition9.com>
2024-11-02 12:26:18 -0700

    Metaframe fetcher also

--- app/libs/src/metapage/util.ts
+++ app/libs/src/metapage/util.ts
@@ -1,12 +1,25 @@
 import fetchRetryWrapper from "fetch-retry";
 import { convertMetapageDefinitionToVersion } from "./conversions";
-import { MetaframeDefinitionV03 } from "./v0_3/all";
-import { MetaframeDefinitionV1 } from "./v1";
-import { VersionsMetapage } from "./versions";
+import { MetaframeDefinitionV1, MetapageDefinitionV1 } from "./v1";
+import { VersionsMetaframe, VersionsMetapage } from "./versions";
+import { MetaframeDefinitionV4, MetapageDefinitionV3 } from "./v0_4";
 
 const fetchRetry = fetchRetryWrapper(fetch);
 
-export const getMetaframeDefinitionFromUrl = async (url: string, version: VersionsMetapage): Promise<MetaframeDefinitionV1|MetaframeDefinitionV03> => {
+export const getMetapageDefinitionFromUrl = async (url: string, version: VersionsMetapage): Promise<MetapageDefinitionV1|MetapageDefinitionV3> => {
+  const metapageUrl = new URL(url);
+  metapageUrl.pathname = metapageUrl.pathname + (metapageUrl.pathname.endsWith("/") ? "metapage.json" : "/metapage.json");
+  const response = await fetchRetry(metapageUrl.href, {
+    redirect: "follow",
+    retries: 3,
+    retryDelay: 1000,
+  });
+  const definition = await response.json();
+  const convertedDefinition = await convertMetapageDefinitionToVersion(definition, version);
+  return convertedDefinition;
+};
+
+export const getMetaframeDefinitionFromUrl = async (url: string, version: VersionsMetaframe): Promise<MetaframeDefinitionV1|MetaframeDefinitionV4> => {
   const metaframeUrl = new URL(url);
   metaframeUrl.pathname = metaframeUrl.pathname + (metaframeUrl.pathname.endsWith("/") ? "metaframe.json" : "/metaframe.json");
   const response = await fetchRetry(metaframeUrl.href, {
@@ -15,6 +28,5 @@ export const getMetaframeDefinitionFromUrl = async (url: string, version: Versio
     retryDelay: 1000,
   });
   const definition = await response.json();
-  const convertedDefinition = await convertMetapageDefinitionToVersion(definition, version);
-  return convertedDefinition;
+  return definition;
 };
