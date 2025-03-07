8985f9c45a2b37fa1f7b16c77c0fa55e49b1054e
Dion Whitehead <dion@metapage.io>
2025-03-06 21:13:29 -0800

    Build compatible with node

--- app/libs/vite.config.ts
+++ app/libs/vite.config.ts
@@ -20,37 +20,65 @@ export default defineConfig(({ mode }) => ({
   },
 
   build: {
-    outDir: "./dist",
-    target: "modules",
-    emptyOutDir: true,
-    sourcemap: true,
-    minify: "esbuild",
-    reportCompressedSize: true,
+    outDir: 'dist',
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      fileName: (format) => `index.${format === 'es' ? 'js' : format}`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'cjs' ? 'index.cjs' : 'index.js'),
     },
+    sourcemap: true,
+    emptyOutDir: true,
+    minify: 'esbuild',
     rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        // Make sure to keep separate files for imports
-        // entryFileNames: '[name].js',
-        // chunkFileNames: '[name]-[hash].js',
-        // assetFileNames: '[name]-[hash][extname]',
-      },
-      external: [],
-      plugins: [
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        typescript({
-          sourceMap: true,
-          declaration: true,
-          outDir: "dist",
-        }),
+      external: [
+        '@metapages/hash-query',
+        'base64-arraybuffer',
+        'compare-versions',
+        'eventemitter3',
+        'fast-json-stable-stringify',
+        'fetch-retry',
+        'mutative',
+        'object-hash',
+        'picomatch-browser',
       ],
     },
+
+
+
+
+
+
+
+    // target: "modules",
+    // emptyOutDir: true,
+    // sourcemap: true,
+    // minify: "esbuild",
+    reportCompressedSize: true,
+    // lib: {
+    //   entry: path.resolve(__dirname, "src/index.ts"),
+    //   formats: ['es', 'cjs'],
+    //   fileName: format => `index.${format === 'cjs' ? 'cjs' : 'js'}`
+    // },
+    // rollupOptions: {
+    //   output: {
+    //     entryFileNames: '[name].js',
+    //     chunkFileNames: '[name].js',
+    //     // Make sure to keep separate files for imports
+    //     // entryFileNames: '[name].js',
+    //     // chunkFileNames: '[name]-[hash].js',
+    //     // assetFileNames: '[name]-[hash][extname]',
+    //   },
+    //   external: [],
+    //   plugins: [
+    //     typescriptPaths({
+    //       preserveExtensions: true,
+    //     }),
+    //     typescript({
+    //       sourceMap: true,
+    //       declaration: true,
+    //       outDir: "dist",
+    //     }),
+    //   ],
+    // },
   },
 }));
